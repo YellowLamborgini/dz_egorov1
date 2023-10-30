e44d788cc02bdc51990e949a0ef51a69037d38da
YellowLamborgini <denisegorov2011@yandex.ru>
2023-10-30 21:27:41 +0300

    Добавьте файлы проекта.

---
+++ dz_egorov1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите число");
+int chislo=Convert.ToInt32(Console.ReadLine());
+if (chislo<1&chislo>100)
+{
+    Console.WriteLine("Ошибка");
+}
+else if (chislo%3==0& chislo %5 != 0)
+{
+    Console.WriteLine("Fizz");
+}
+else if (chislo % 5 == 0&chislo % 3 != 0)
+{
+    Console.WriteLine("Buzz");
+}
+else if (chislo % 5 == 0 & chislo % 3 == 0)
+{
+    Console.WriteLine("Fizz Buzz");
+}
+else
+{
+    Console.WriteLine($"Число:{ chislo}");
+}
